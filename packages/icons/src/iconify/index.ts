161768a89f01cc3c1f90c694ce3fc2ac4cbe602b
dap <15891557205@163.com>
2024-08-18 10:05:57 +0800

    chore: 添加图标

--- packages/icons/src/iconify/index.ts
+++ packages/icons/src/iconify/index.ts
@@ -11,3 +11,13 @@ export const MdiGithub = createIconifyIcon('mdi:github');
 export const MdiGoogle = createIconifyIcon('mdi:google');
 
 export const MdiQqchat = createIconifyIcon('mdi:qqchat');
+
+export const EosSystem = createIconifyIcon('eos-icons:system-group');
+// 缓存监控使用
+export const RedisIcon = createIconifyIcon('logos:redis');
+export const CommandLineIcon = createIconifyIcon(
+  'flat-color-icons:command-line',
+);
+export const MemoryIcon = createIconifyIcon('la:memory');
+
+export const GiteeIcon = createIconifyIcon('simple-icons:gitee');
