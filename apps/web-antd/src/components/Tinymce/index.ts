6c43d818a008a3d4659c2bfb66c378c6e9ffb9b0
dap <15891557205@163.com>
2024-08-20 10:17:48 +0800

    feat: 移植TinyMCe组件

---
+++ apps/web-antd/src/components/Tinymce/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '#/utils';
+
+import tinymce from './src/Editor.vue';
+
+export const Tinymce = withInstall(tinymce);
---
+++ apps/web-antd/src/views/演示使用自行删除/tinymce/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+
+import { Tinymce } from '#/components/Tinymce';
+
+const readonly = ref(false);
+const content = ref('');
+</script>
+
+<template>
+  <Page title="Tinymce富文本">
+    <div class="flex flex-col gap-[16px]">
+      <div class="flex items-center gap-[16px]">
+        <span>只读</span>
+        <a-switch v-model:checked="readonly" />
+      </div>
+      <Tinymce v-model:value="content" :options="{ readonly }" />
+    </div>
+  </Page>
+</template>
