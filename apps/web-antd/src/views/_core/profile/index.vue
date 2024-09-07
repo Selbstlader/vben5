51770ad6a05999729c9b12e146ff631d4d797086
dap <15891557205@163.com>
2024-09-07 20:21:20 +0800

    fix: 头像更改 store更新

--- apps/web-antd/src/views/_core/profile/index.vue
+++ apps/web-antd/src/views/_core/profile/index.vue
@@ -4,8 +4,10 @@ import type { UserProfile } from '#/api/system/profile/model';
 import { onMounted, ref } from 'vue';
 
 import { Page } from '@vben/common-ui';
+import { useUserStore } from '@vben/stores';
 
 import { userProfile } from '#/api/system/profile';
+import { useAuthStore } from '#/store';
 
 import ProfilePanel from './profile-panel.vue';
 import SettingPanel from './setting-panel.vue';
@@ -17,13 +19,26 @@ async function loadProfile() {
 }
 
 onMounted(loadProfile);
+
+const authStore = useAuthStore();
+const userStore = useUserStore();
+/**
+ * ToDo 接口重复
+ */
+async function handleUploadFinish() {
+  // 重新加载用户信息
+  await loadProfile();
+  // 更新store
+  const userInfo = await authStore.fetchUserInfo();
+  userStore.setUserInfo(userInfo);
+}
 </script>
 
 <template>
   <Page>
     <div class="flex flex-col gap-[16px] lg:flex-row">
       <!-- 左侧 -->
-      <ProfilePanel :profile="profile" @upload-finish="loadProfile" />
+      <ProfilePanel :profile="profile" @upload-finish="handleUploadFinish" />
       <!-- 右侧 -->
       <SettingPanel v-if="profile" :profile="profile" @reload="loadProfile" />
     </div>
