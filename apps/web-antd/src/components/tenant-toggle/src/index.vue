c3e66c0aec7f1796e820d7b36a0d16b6e6eea1ae
dap <15891557205@163.com>
2024-09-14 08:34:45 +0800

    feat: 租户选中增加样式

--- apps/web-antd/src/components/tenant-toggle/src/index.vue
+++ apps/web-antd/src/components/tenant-toggle/src/index.vue
@@ -113,3 +113,16 @@ function filterOption(input: string, option: TenantOption) {
     </Select>
   </div>
 </template>
+
+<style lang="scss" scoped>
+// 当选中时 添加border样式
+:deep(.ant-select-selector) {
+  &:has(.ant-select-selection-item) {
+    box-shadow: 0 0 10px hsl(var(--primary));
+  }
+  // 选中时字体加粗
+  .ant-select-selection-item {
+    font-weight: 600;
+  }
+}
+</style>
