719ebbcf374bda9bc94397df19188e2e35ed4275
dap <15891557205@163.com>
2024-09-23 11:33:55 +0800

    feat: 部门管理

---
+++ apps/web-antd/src/views/system/dept/data.ts
@@ -0,0 +1,99 @@
+import { DictEnum } from '@vben/constants';
+import { getPopupContainer } from '@vben/utils';
+
+import { type FormSchemaGetter, z } from '#/adapter';
+import { getDictOptions } from '#/utils/dict';
+
+export const drawerSchema: FormSchemaGetter = () => [
+  {
+    component: 'Input',
+    dependencies: {
+      show: () => false,
+      triggerFields: [''],
+    },
+    fieldName: 'deptId',
+  },
+  {
+    component: 'TreeSelect',
+    componentProps: {
+      getPopupContainer,
+      placeholder: '请选择',
+    },
+    dependencies: {
+      show: (model) => model.parentId !== 0,
+      triggerFields: ['parentId'],
+    },
+    fieldName: 'parentId',
+    label: '上级部门',
+    rules: 'selectRequired',
+  },
+  {
+    component: 'Input',
+    componentProps: {
+      placeholder: '请输入',
+    },
+    fieldName: 'deptName',
+    label: '部门名称',
+    rules: 'required',
+  },
+  {
+    component: 'InputNumber',
+    componentProps: {
+      placeholder: '请输入',
+    },
+    fieldName: 'orderNum',
+    label: '显示排序',
+    rules: 'required',
+  },
+  {
+    component: 'Input',
+    componentProps: {
+      placeholder: '请输入',
+    },
+    fieldName: 'deptCategory',
+    label: '类别编码',
+  },
+  {
+    component: 'Select',
+    componentProps: {
+      // 选中了就只能修改 不能重置为无负责人
+      allowClear: false,
+      getPopupContainer,
+      placeholder: '请选择',
+    },
+    fieldName: 'leader',
+    label: '负责人',
+  },
+  {
+    component: 'Input',
+    componentProps: {
+      placeholder: '请输入',
+    },
+    fieldName: 'phone',
+    label: '联系电话',
+    rules: z
+      .string()
+      .regex(/^\d{1,3}-\d{8,11}$/, { message: '请输入正确的手机号' })
+      .optional(),
+  },
+  {
+    component: 'Input',
+    componentProps: {
+      placeholder: '请输入',
+    },
+    fieldName: 'email',
+    label: '邮箱',
+    rules: z.string().email({ message: '请输入正确的邮箱' }).optional(),
+  },
+  {
+    component: 'RadioGroup',
+    componentProps: {
+      buttonStyle: 'solid',
+      options: getDictOptions(DictEnum.SYS_NORMAL_DISABLE),
+      optionType: 'button',
+    },
+    defaultValue: '0',
+    fieldName: 'status',
+    label: '状态',
+  },
+];
--- apps/web-antd/src/views/system/dept/index.vue
+++ apps/web-antd/src/views/system/dept/index.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import CommonSkeleton from '#/views/common';
+import { Page, useVbenDrawer } from '@vben/common-ui';
+import { $t } from '@vben/locales';
+
+import { Space } from 'ant-design-vue';
+
+import deptDrawer from './dept-drawer.vue';
+
+const [DeptDrawer, drawerApi] = useVbenDrawer({
+  connectedComponent: deptDrawer,
+});
+
+function handleAdd() {
+  drawerApi.setData({});
+  drawerApi.open();
+}
+
+function handleTest(id: number | string) {
+  drawerApi.setData({ id });
+  drawerApi.open();
+}
 </script>
 
 <template>
-  <div>
-    <CommonSkeleton />
-  </div>
+  <Page>
+    <Space>
+      <a-button type="primary" @click="handleAdd">
+        {{ $t('pages.common.add') }}
+      </a-button>
+      <a-button @click="handleTest(103)"> 新增 上级id=103 </a-button>
+      <a-button @click="handleTest(105)"> 新增 上级id=105 </a-button>
+    </Space>
+    <DeptDrawer />
+  </Page>
 </template>
