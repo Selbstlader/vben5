45d4365de725aecc2a5b0c65937723c5e24a5a6a
dap <15891557205@163.com>
2024-10-05 17:02:25 +0800

    feat: 客户端管理

--- apps/web-antd/src/views/system/client/data.tsx
+++ apps/web-antd/src/views/system/client/data.tsx
@@ -1,9 +1,10 @@
-import type { FormSchemaGetter } from '#/adapter';
+import type { FormSchemaGetter, VxeGridProps } from '#/adapter';
 
 import { DictEnum } from '@vben/constants';
 import { getPopupContainer } from '@vben/utils';
 
-import { getDictOptions } from '#/utils/dict';
+import { getDict, getDictOptions } from '#/utils/dict';
+import { renderDict, renderDictTags } from '#/utils/render';
 
 export const querySchema: FormSchemaGetter = () => [
   {
@@ -23,6 +24,75 @@ export const querySchema: FormSchemaGetter = () => [
     },
     fieldName: 'status',
     label: '状态',
+  },
+];
+
+export const columns: VxeGridProps['columns'] = [
+  { type: 'checkbox', width: 60 },
+  {
+    title: '客户端ID',
+    field: 'clientId',
+    showOverflow: true,
+  },
+  {
+    title: '客户端key',
+    field: 'clientKey',
+  },
+  {
+    title: '客户端密钥',
+    field: 'clientSecret',
+  },
+  {
+    title: '授权类型',
+    field: 'grantTypeList',
+    slots: {
+      default: ({ row }) => {
+        if (!row.grantTypeList) {
+          return '无';
+        }
+        return renderDictTags(
+          row.grantTypeList,
+          getDict(DictEnum.SYS_GRANT_TYPE),
+        );
+      },
+    },
+  },
+  {
+    title: '设备类型',
+    field: 'deviceType',
+    slots: {
+      default: ({ row }) => {
+        return renderDict(row.deviceType, DictEnum.SYS_DEVICE_TYPE);
+      },
+    },
+  },
+  {
+    title: 'token活跃时间',
+    field: 'activeTimeout',
+    formatter({ row }) {
+      return `${row.activeTimeout}秒`;
+    },
+  },
+  {
+    title: 'token超时时间',
+    field: 'timeout',
+    formatter({ row }) {
+      return `${row.timeout}秒`;
+    },
+  },
+  {
+    title: '状态',
+    field: 'status',
+    slots: {
+      default: 'status',
+    },
+  },
+  {
+    field: 'action',
+    fixed: 'right',
+    slots: { default: 'action' },
+    title: '操作',
+    width: 180,
   },
 ];
 
