da4c612481eb5e1447fc73aeb849a80378a56eba
dap <15891557205@163.com>
2024-10-06 09:37:57 +0800

    chore: reset func

--- apps/web-antd/src/views/system/user/index.vue
+++ apps/web-antd/src/views/system/user/index.vue
@@ -60,6 +60,11 @@ const formOptions: VbenFormProps = {
     labelWidth: 80,
   },
   wrapperClass: 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4',
+  handleReset: async () => {
+    selectDeptId.value = [];
+    // eslint-disable-next-line no-use-before-define
+    await tableApi.query();
+  },
 };
 
 const gridOptions: VxeGridProps = {
