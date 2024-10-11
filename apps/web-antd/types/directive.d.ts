fbbb02397148963099e5f3c2dc6097d8afaa149e
dap <15891557205@163.com>
2024-10-11 21:05:25 +0800

    chore: 注释

--- apps/web-antd/types/directive.d.ts
+++ apps/web-antd/types/directive.d.ts
@@ -5,7 +5,8 @@ declare module 'vue' {
     /**
      * 判断权限: v-access:code=""
      * 判断角色  v-access:role=""
-     * TODO: 泛型第四个函数为code或role 不生效
+     * VueOfficial插件暂时不支持判断modifer/arg
+     * @see https://github.com/vuejs/language-tools/issues/4810
      */
     vAccess: Directive<Element, string | string[]>;
   }
