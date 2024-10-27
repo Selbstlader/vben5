3be5f90e62577970eef4fe9ddf1e85e7c157b3ea
dap <15891557205@163.com>
2024-10-27 20:52:23 +0800

    chore: v-access的类型校验

--- apps/web-antd/types/directive.d.ts
+++ apps/web-antd/types/directive.d.ts
@@ -5,9 +5,8 @@ declare module 'vue' {
     /**
      * 判断权限: v-access:code=""
      * 判断角色  v-access:role=""
-     * VueOfficial插件暂时不支持判断modifer/arg
-     * @see https://github.com/vuejs/language-tools/issues/4810
+     * 需要VueOfficial插件版本 >= 2.1.8
      */
-    vAccess: Directive<Element, string | string[]>;
+    vAccess: Directive<Element, string | string[], string, 'code' | 'role'>;
   }
 }
