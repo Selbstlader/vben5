272777f84fe46a9abb8f01585719418e243cb599
dap <15891557205@163.com>
2024-11-06 17:04:10 +0800

    feat: 系统/浏览器离线图标

--- packages/icons/src/iconify-offline/index.ts
+++ packages/icons/src/iconify-offline/index.ts
@@ -3,15 +3,33 @@ import { createIconifyOfflineIcon } from '@vben-core/icons';
 import githubOutlined from '@iconify/icons-ant-design/github-outlined';
 import inboxIcon from '@iconify/icons-ant-design/inbox-outlined';
 import userOutlined from '@iconify/icons-ant-design/user-outlined';
+import ucIcon from '@iconify/icons-arcticons/uc-browser';
+import linuxIcon from '@iconify/icons-devicon/linux';
+import windowsIcon from '@iconify/icons-devicon/windows8';
 import alipayIcon from '@iconify/icons-fa-brands/alipay';
+import androidIcon from '@iconify/icons-flat-color-icons/android-os';
 import comandLine from '@iconify/icons-flat-color-icons/command-line';
+import defaultOsIcon from '@iconify/icons-ic/outline-computer';
 import memoryIcon from '@iconify/icons-la/memory';
+import chromeIcon from '@iconify/icons-logos/chrome';
+import firefoxIcon from '@iconify/icons-logos/firefox';
+import edgeIcon from '@iconify/icons-logos/microsoft-edge';
+import operaIcon from '@iconify/icons-logos/opera';
+import quarkIcon from '@iconify/icons-logos/quarkus-icon';
 import redisIcon from '@iconify/icons-logos/redis';
+import safariIcon from '@iconify/icons-logos/safari';
+import iphoneIcon from '@iconify/icons-majesticons/iphone-x-apps-line';
+import micromessengerIcon from '@iconify/icons-mdi/wechat';
+import defaultBrowserIcon from '@iconify/icons-ph/browser-duotone';
+import baiduIcon from '@iconify/icons-ri/baidu-fill';
 import dingdingFill from '@iconify/icons-ri/dingding-fill';
+import dingtalkIcon from '@iconify/icons-ri/dingding-line';
 import taobaoIconFill from '@iconify/icons-ri/taobao-fill';
 import giteeIcon from '@iconify/icons-simple-icons/gitee';
+import qqIcon from '@iconify/icons-simple-icons/tencentqq';
 import githubOAuthIcon from '@iconify/icons-uiw/github';
 import excelIcon from '@iconify/icons-vscode-icons/file-type-excel';
+import osxIcon from '@iconify/icons-wpf/macos';
 
 // 用户 下拉菜单
 export const GitHubOutlined = createIconifyOfflineIcon(
@@ -64,4 +82,61 @@ export const GiteeIcon = createIconifyOfflineIcon(
 export const GithubOAuthIcon = createIconifyOfflineIcon(
   'uiw:github',
   githubOAuthIcon,
+);
+
+// 系统相关图标
+export const WindowsIcon = createIconifyOfflineIcon(
+  'devicon:windows8',
+  windowsIcon,
+);
+export const LinuxIcon = createIconifyOfflineIcon('devicon:linux', linuxIcon);
+export const OSXIcon = createIconifyOfflineIcon('wpf:macos', osxIcon);
+export const AndroidIcon = createIconifyOfflineIcon(
+  'flat-color-icons:android-os',
+  androidIcon,
+);
+export const IPhoneIcon = createIconifyOfflineIcon(
+  'majesticons:iphone-x-apps-line',
+  iphoneIcon,
+);
+// 上面图标没找到 默认图标
+export const DefaultOsIcon = createIconifyOfflineIcon(
+  'ic:outline-computer',
+  defaultOsIcon,
+);
+
+// 浏览器相关图标
+export const ChromeIcon = createIconifyOfflineIcon('logos:chrome', chromeIcon);
+export const EdgeIcon = createIconifyOfflineIcon(
+  'logos:microsoft-edge',
+  edgeIcon,
+);
+export const FirefoxIcon = createIconifyOfflineIcon(
+  'logos:firefox',
+  firefoxIcon,
+);
+export const OperaIcon = createIconifyOfflineIcon('logos:opera', operaIcon);
+export const SafariIcon = createIconifyOfflineIcon('logos:safari', safariIcon);
+export const MicromessengerIcon = createIconifyOfflineIcon(
+  'mdi:wechat',
+  micromessengerIcon,
+);
+export const QuarkIcon = createIconifyOfflineIcon(
+  'logos:quarkus-icon',
+  quarkIcon,
+);
+export const QQIcon = createIconifyOfflineIcon(
+  'simple-icons:tencentqq',
+  qqIcon,
+);
+export const DingtalkIcon = createIconifyOfflineIcon(
+  'ri:dingding-line',
+  dingtalkIcon,
+);
+export const UcIcon = createIconifyOfflineIcon('arcticons:uc-browser', ucIcon);
+export const BaiduIcon = createIconifyOfflineIcon('ri:baidu-fill', baiduIcon);
+// 未知浏览器图标
+export const DefaultBrowserIcon = createIconifyOfflineIcon(
+  'ph:browser-duotone',
+  defaultBrowserIcon,
 );
