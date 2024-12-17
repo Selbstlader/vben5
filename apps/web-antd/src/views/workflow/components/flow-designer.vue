395b45fa48965a33b9c494f80a94555f1e27d1ad
dap <15891557205@163.com>
2024-12-17 08:09:47 +0800

    chore: 需要加上clientId

--- apps/web-antd/src/views/workflow/components/flow-designer.vue
+++ apps/web-antd/src/views/workflow/components/flow-designer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
 
+import { useAppConfig } from '@vben/hooks';
 import { stringify } from '@vben/request';
 import { useAccessStore } from '@vben/stores';
 
@@ -10,10 +11,13 @@ const route = useRoute();
 const definitionId = route.query.definitionId as string;
 const disabled = route.query.disabled === 'true';
 
+const { clientId } = useAppConfig(import.meta.env, import.meta.env.PROD);
+
 const accessStore = useAccessStore();
 const params = {
   Authorization: `Bearer ${accessStore.accessToken}`,
   id: definitionId,
+  clientId,
   disabled,
 };
 
