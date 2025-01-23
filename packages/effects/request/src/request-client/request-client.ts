463d50328fb627044b0710914948ae950f60813d
dap <15891557205@163.com>
2025-01-23 15:15:20 +0800

    fix: missing type

--- packages/effects/request/src/request-client/request-client.ts
+++ packages/effects/request/src/request-client/request-client.ts
@@ -1,4 +1,4 @@
-import type { AxiosInstance, AxiosResponse } from 'axios';
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import type { RequestClientConfig, RequestClientOptions } from './types';
 
