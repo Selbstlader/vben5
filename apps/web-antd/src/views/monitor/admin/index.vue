64d3a2115307136d56cf1552d527c47668d0fa03
dap <15891557205@163.com>
2025-01-26 17:35:09 +0800

    refactor: update url

--- apps/web-antd/src/views/monitor/admin/index.vue
+++ apps/web-antd/src/views/monitor/admin/index.vue
@@ -1,6 +1,3 @@
 <template>
-  <iframe
-    class="size-full"
-    src="http://localhost:9090/admin/applications"
-  ></iframe>
+  <iframe class="size-full" src="http://localhost:9090/applications"></iframe>
 </template>
