f78bc4e4f7b27c8d56227499a6bdaa27cd99bbc0
dap <15891557205@163.com>
2025-02-07 14:42:47 +0800

    chore: 修改路径

--- apps/web-antd/src/views/monitor/admin/index.vue
+++ apps/web-antd/src/views/monitor/admin/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <iframe class="size-full" src="http://localhost:9090/applications"></iframe>
+  <iframe
+    class="size-full"
+    src="http://localhost:9090/admin/applications"
+  ></iframe>
 </template>
