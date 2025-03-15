7230b94b16e6ef45daf110aa3afee3b5bbcc2427
dap <15891557205@163.com>
2025-03-15 17:05:45 +0800

    style: 登录页input/select样式

--- packages/@core/ui-kit/shadcn-ui/src/ui/select/SelectTrigger.vue
+++ packages/@core/ui-kit/shadcn-ui/src/ui/select/SelectTrigger.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import type { SelectTriggerProps } from 'radix-vue';
 
-import { cn } from '@vben-core/shared/utils';
-import { ChevronDown } from 'lucide-vue-next';
-import { SelectIcon, SelectTrigger, useForwardProps } from 'radix-vue';
 import { computed } from 'vue';
 
-const props = defineProps<{ class?: any } & SelectTriggerProps>();
+import { cn } from '@vben-core/shared/utils';
+
+import { ChevronDown } from 'lucide-vue-next';
+import { SelectIcon, SelectTrigger, useForwardProps } from 'radix-vue';
+
+const props = defineProps<SelectTriggerProps & { class?: any }>();
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props;
@@ -22,7 +24,7 @@ const forwardedProps = useForwardProps(delegatedProps);
     v-bind="forwardedProps"
     :class="
       cn(
-        'border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-10 w-full items-center justify-between whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+        'border-input ring-offset-background placeholder:text-muted-foreground flex h-10 w-full items-center justify-between whitespace-nowrap rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
         props.class,
       )
     "
