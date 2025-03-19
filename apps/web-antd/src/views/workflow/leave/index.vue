9e4f8861975af5bf45f3001ad0436430b07e8479
dap <15891557205@163.com>
2025-03-19 15:40:17 +0800

    fix: 草稿不允许查看

--- apps/web-antd/src/views/workflow/leave/index.vue
+++ apps/web-antd/src/views/workflow/leave/index.vue
@@ -174,7 +174,12 @@ function handleInfo(row: Required<LeaveForm>) {
               撤销
             </ghost-button>
           </Popconfirm>
-          <ghost-button @click="handleInfo(row)">详情</ghost-button>
+          <ghost-button
+            v-if="['draft'].includes(row.status)"
+            @click="handleInfo(row)"
+          >
+            详情
+          </ghost-button>
           <Popconfirm
             :get-popup-container="getVxePopupContainer"
             placement="left"
