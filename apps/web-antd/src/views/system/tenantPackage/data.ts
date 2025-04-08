ba8f36a2c0354ea54fed14ebd3f6344486421ac9
dap <15891557205@163.com>
2025-04-08 11:04:12 +0800

    update: 移除老版本的不需要组件/代码

--- apps/web-antd/src/views/system/tenantPackage/data.ts
+++ apps/web-antd/src/views/system/tenantPackage/data.ts
@@ -68,8 +68,3 @@ export const drawerSchema: FormSchemaGetter = () => [
     label: '备注',
   },
 ];
-
-// 租户管理 不可分配  只有superadmin有权限操作 分配了也没用
-export const excludeIds = [
-  6, 121, 122, 1606, 1607, 1608, 1609, 1610, 1611, 1612, 1613, 1614, 1615,
-];
